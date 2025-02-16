f0c20e95548ee287409d815a155dafebabebb449
osamamohti2002 <99078852@mydavinci.nl>
2025-02-16 13:10:46 +0100

    push

---
+++ PokemonBattle/classes/pokeball.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+
+public class Getname
+{
+    public string name;
+
+
+    public Getname(string Name)
+    {
+        this.name = Name;
+    }
+
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Getname myName = new Getname("Lithe");
+        Console.WriteLine(myName);
+    }
+}
+
