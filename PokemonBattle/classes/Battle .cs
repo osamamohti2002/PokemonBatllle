4c8483f190ff518091069ed80c0eaee646b747ce
osamamohti2002 <99078852@mydavinci.nl>
2025-03-14 14:13:39 +0100

    push

---
+++ PokemonBattle/classes/Battle .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonBattle.classes
+{
+    class Battle
+    {
+        Trainer trainer1;
+        Trainer trainer2;
+        int aantalRounds;
+
+        public Battle(Trainer trainer1, Trainer trainer2, int aantalRounds)
+        {
+
+        }
+
+    }
+
+
+}
